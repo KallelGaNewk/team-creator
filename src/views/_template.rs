33c18749ccd997c896253b141635542823c4c8cc
Ian Kallel <kallelgn@gmail.com>
2025-11-08 00:28:13 -0300

    Add a template view

---
+++ src/views/_template.rs
@@ -0,0 +1,35 @@
+use crate::cache::PersistentCache;
+use eframe::egui;
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct PersistentData {}
+
+impl PersistentCache for PersistentData {
+    fn filename() -> &'static str {
+        "template_cache.ron"
+    }
+}
+
+pub struct Settings {
+    persistent_data: PersistentData,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            persistent_data: PersistentData::read_or(PersistentData {}),
+        }
+    }
+}
+
+impl super::View for Settings {
+    fn name(&self) -> &str {
+        "📄 Template"
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        ui.heading("Template View");
+    }
+}
+
+impl Settings {}
